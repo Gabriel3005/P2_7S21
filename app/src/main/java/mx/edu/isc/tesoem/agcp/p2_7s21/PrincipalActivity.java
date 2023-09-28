d08c902faa096e117a2f8090b20dd778dfb17b6f
Alejandro <alexgabo.c.p@gmail.com>
2023-09-28 14:58:32 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/agcp/p2_7s21/PrincipalActivity.java
@@ -0,0 +1,25 @@
+package mx.edu.isc.tesoem.agcp.p2_7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent lanza = new Intent(PrincipalActivity.this, SegundaActivity.class);
+                startActivity(lanza);
+                finish();
+            }
+        }, 4000);
+    }
+}
